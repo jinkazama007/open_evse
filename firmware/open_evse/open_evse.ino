6f10a1ffedffce07543ae0843f6d405f1320fc44
scl <lincomatic@gmail.com>
2020-03-27 18:07:00 -0700

    add $FF B to enable/disable front button

--- firmware/open_evse/open_evse.ino
+++ firmware/open_evse/open_evse.ino
@@ -2203,6 +2203,8 @@ int8_t BtnHandler::DoShortPress(int8_t infaultstate)
 
 void BtnHandler::ChkBtn()
 {
+  if (!g_EvseController.ButtonIsEnabled()) return;
+
   WDT_RESET();
 
   int8_t infaultstate = g_EvseController.InFaultState();
